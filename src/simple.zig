e8eb269963ed06e5c855ced6094066b216f17ef6
ItsMeSamey <sameychain5041@gmail.com>
2025-06-22 11:18:05 +0530

    moved some code

--- src/simple.zig
+++ src/simple.zig
@@ -719,126 +719,7 @@ pub fn ToMergedT(T: type, context: Context) type {
 // ========================================
 
 const testing = std.testing;
-
-fn expectEqual(expected: anytype, actual: anytype) !void {
-  const print = std.debug.print;
-
-  switch (@typeInfo(@TypeOf(actual))) {
-    .noreturn, .@"opaque", .frame, .@"anyframe", => @compileError("value of type " ++ @typeName(@TypeOf(actual)) ++ " encountered"),
-
-    .void => return,
-
-    .type => {
-      if (actual != expected) {
-        print("expected type {s}, found type {s}\n", .{ @typeName(expected), @typeName(actual) });
-        return error.TestExpectedEqual;
-      }
-    },
-
-    .bool, .int, .float, .comptime_float, .comptime_int, .enum_literal, .@"enum", .@"fn", .error_set => {
-      if (actual != expected) {
-        print("expected {}, found {}\n", .{ expected, actual });
-        return error.TestExpectedEqual;
-      }
-    },
-
-    .pointer => |pointer| {
-      switch (pointer.size) {
-        .one, .many, .c => {
-          if (actual == expected) return;
-          return expectEqual(actual.*, expected.*);
-        },
-        .slice => {
-          if (actual.len != expected.len) {
-            print("expected slice len {}, found {}\n", .{ expected.len, actual.len });
-            return error.TestExpectedEqual;
-          }
-          if (actual.ptr == expected.ptr) return;
-          for (actual, expected, 0..) |va, ve, i| {
-            expectEqual(va, ve) catch |e| {
-              print("index {d} incorrect. expected {any}, found {any}\n", .{ i, expected[i], actual[i] });
-              return e;
-            };
-          }
-        },
-      }
-    },
-
-    .array => |array| {
-      inline for (0..array.len) |i| {
-        expectEqual(expected[i], actual[i]) catch |e| {
-          print("index {d} incorrect. expected {any}, found {any}\n", .{ i, expected[i], actual[i] });
-          return e;
-        };
-      }
-    },
-
-    .vector => |info| {
-      var i: usize = 0;
-      while (i < info.len) : (i += 1) {
-        if (!std.meta.eql(expected[i], actual[i])) {
-          print("index {d} incorrect. expected {any}, found {any}\n", .{ i, expected[i], actual[i] });
-          return error.TestExpectedEqual;
-        }
-      }
-    },
-
-    .@"struct" => |structType| {
-      inline for (structType.fields) |field| {
-        try expectEqual(@field(expected, field.name), @field(actual, field.name));
-      }
-    },
-
-    .@"union" => |union_info| {
-      if (union_info.tag_type == null) @compileError("Unable to compare untagged union values for type " ++ @typeName(@TypeOf(actual)));
-      const Tag = std.meta.Tag(@TypeOf(expected));
-      const expectedTag = @as(Tag, expected);
-      const actualTag = @as(Tag, actual);
-
-      try expectEqual(expectedTag, actualTag);
-
-      switch (expected) {
-        inline else => |val, tag| try expectEqual(val, @field(actual, @tagName(tag))),
-      }
-    },
-
-    .optional => {
-      if (expected) |expected_payload| {
-        if (actual) |actual_payload| {
-          try expectEqual(expected_payload, actual_payload);
-        } else {
-          print("expected {any}, found null\n", .{expected_payload});
-          return error.TestExpectedEqual;
-        }
-      } else {
-        if (actual) |actual_payload| {
-          print("expected null, found {any}\n", .{actual_payload});
-          return error.TestExpectedEqual;
-        }
-      }
-    },
-
-    .error_union => {
-      if (expected) |expected_payload| {
-        if (actual) |actual_payload| {
-          try expectEqual(expected_payload, actual_payload);
-        } else |actual_err| {
-          print("expected {any}, found {}\n", .{ expected_payload, actual_err });
-          return error.TestExpectedEqual;
-        }
-      } else |expected_err| {
-        if (actual) |actual_payload| {
-          print("expected {}, found {any}\n", .{ expected_err, actual_payload });
-          return error.TestExpectedEqual;
-        } else |actual_err| {
-          try expectEqual(expected_err, actual_err);
-        }
-      }
-    },
-
-    else => @compileError("Unsupported type in expectEqual: " ++ @typeName(@TypeOf(expected))),
-  }
-}
+const expectEqual = @import("testing.zig").expectEqual;
 
 fn _testMergingDemerging(value: anytype, comptime options: ToMergedOptions) !void {
   const T = @TypeOf(value);
@@ -1288,21 +1169,21 @@ test "deep optional and pointer nesting" {
   try testMerging(DeepOptional{ .val = @as(??*const u32, null) });
 }
 
-// test "recursion limit with dereference" {
-//   const Node = struct {
-//     payload: u32,
-//     next: ?*const @This(),
-//   };
-//
-//   const n3 = Node{ .payload = 3, .next = null };
-//   const n2 = Node{ .payload = 2, .next = &n3 };
-//   const n1 = Node{ .payload = 1, .next = &n2 };
-//
-//   // This should only serialize n1 and the pointer to n2. 
-//   // The `write` for n2 will hit the dereference limit and treat it as a direct (raw pointer) value.
-//   try _testMergingDemerging(n1, .{ .T = Node, .dereference = 1 });
-// }
-//
+test "recursion limit with dereference" {
+  const Node = struct {
+    payload: u32,
+    next: ?*const @This(),
+  };
+
+  const n3 = Node{ .payload = 3, .next = null };
+  const n2 = Node{ .payload = 2, .next = &n3 };
+  const n1 = Node{ .payload = 1, .next = &n2 };
+
+  // This should only serialize n1 and the pointer to n2. 
+  // The `write` for n2 will hit the dereference limit and treat it as a direct (raw pointer) value.
+  try _testMergingDemerging(n1, .{ .T = Node, .flatten_self_references = 1, .recursive_rereferencing = .disallow });
+}
+
 // test "recursive type merging" {
 //   const Node = struct {
 //     payload: u32,
---
+++ src/testing.zig
@@ -0,0 +1,122 @@
+const std = @import("std");
+
+pub fn expectEqual(expected: anytype, actual: anytype) !void {
+  const print = std.debug.print;
+
+  switch (@typeInfo(@TypeOf(actual))) {
+    .noreturn, .@"opaque", .frame, .@"anyframe", => @compileError("value of type " ++ @typeName(@TypeOf(actual)) ++ " encountered"),
+
+    .void => return,
+
+    .type => {
+      if (actual != expected) {
+        print("expected type {s}, found type {s}\n", .{ @typeName(expected), @typeName(actual) });
+        return error.TestExpectedEqual;
+      }
+    },
+
+    .bool, .int, .float, .comptime_float, .comptime_int, .enum_literal, .@"enum", .@"fn", .error_set => {
+      if (actual != expected) {
+        print("expected {}, found {}\n", .{ expected, actual });
+        return error.TestExpectedEqual;
+      }
+    },
+
+    .pointer => |pointer| {
+      switch (pointer.size) {
+        .one, .many, .c => {
+          if (actual == expected) return;
+          return expectEqual(actual.*, expected.*);
+        },
+        .slice => {
+          if (actual.len != expected.len) {
+            print("expected slice len {}, found {}\n", .{ expected.len, actual.len });
+            return error.TestExpectedEqual;
+          }
+          if (actual.ptr == expected.ptr) return;
+          for (actual, expected, 0..) |va, ve, i| {
+            expectEqual(va, ve) catch |e| {
+              print("index {d} incorrect. expected {any}, found {any}\n", .{ i, expected[i], actual[i] });
+              return e;
+            };
+          }
+        },
+      }
+    },
+
+    .array => |array| {
+      inline for (0..array.len) |i| {
+        expectEqual(expected[i], actual[i]) catch |e| {
+          print("index {d} incorrect. expected {any}, found {any}\n", .{ i, expected[i], actual[i] });
+          return e;
+        };
+      }
+    },
+
+    .vector => |info| {
+      var i: usize = 0;
+      while (i < info.len) : (i += 1) {
+        if (!std.meta.eql(expected[i], actual[i])) {
+          print("index {d} incorrect. expected {any}, found {any}\n", .{ i, expected[i], actual[i] });
+          return error.TestExpectedEqual;
+        }
+      }
+    },
+
+    .@"struct" => |structType| {
+      inline for (structType.fields) |field| {
+        try expectEqual(@field(expected, field.name), @field(actual, field.name));
+      }
+    },
+
+    .@"union" => |union_info| {
+      if (union_info.tag_type == null) @compileError("Unable to compare untagged union values for type " ++ @typeName(@TypeOf(actual)));
+      const Tag = std.meta.Tag(@TypeOf(expected));
+      const expectedTag = @as(Tag, expected);
+      const actualTag = @as(Tag, actual);
+
+      try expectEqual(expectedTag, actualTag);
+
+      switch (expected) {
+        inline else => |val, tag| try expectEqual(val, @field(actual, @tagName(tag))),
+      }
+    },
+
+    .optional => {
+      if (expected) |expected_payload| {
+        if (actual) |actual_payload| {
+          try expectEqual(expected_payload, actual_payload);
+        } else {
+          print("expected {any}, found null\n", .{expected_payload});
+          return error.TestExpectedEqual;
+        }
+      } else {
+        if (actual) |actual_payload| {
+          print("expected null, found {any}\n", .{actual_payload});
+          return error.TestExpectedEqual;
+        }
+      }
+    },
+
+    .error_union => {
+      if (expected) |expected_payload| {
+        if (actual) |actual_payload| {
+          try expectEqual(expected_payload, actual_payload);
+        } else |actual_err| {
+          print("expected {any}, found {}\n", .{ expected_payload, actual_err });
+          return error.TestExpectedEqual;
+        }
+      } else |expected_err| {
+        if (actual) |actual_payload| {
+          print("expected {}, found {any}\n", .{ expected_err, actual_payload });
+          return error.TestExpectedEqual;
+        } else |actual_err| {
+          try expectEqual(expected_err, actual_err);
+        }
+      }
+    },
+
+    else => @compileError("Unsupported type in expectEqual: " ++ @typeName(@TypeOf(expected))),
+  }
+}
+
