70a5fe29cda87a91620a2fb97ed90e37a4f7d228
ItsMeSamey <sameychain5041@gmail.com>
2025-06-23 05:03:14 +0530

    added wrapper export to root file

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,10 @@
 const std = @import("std");
 pub const meta = @import("meta.zig");
+pub const simple = @import("simple.zig");
+
+/// Options to control the behavior of the `SimpleWrapper` function.
+pub const SimpleOptions = simple.ToMergedOptions;
+
+/// A generic wrapper that manages the memory for a merged object.
+pub const SimpleWrapper = simple.Wrapper;
 
